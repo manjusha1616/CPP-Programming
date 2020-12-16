9988a1be0a3c4c1d1bee8e292d00e79695526c21
manjusha1616 <70739885+manjusha1616@users.noreply.github.com>
2020-12-16 14:24:36 +0530

    Add files via upload

---
+++ Inheritance/04.According To Access Specifiers - PUBLIC Inheritance.cpp
@@ -0,0 +1,84 @@
+//According To Access Specifiers - PUBLIC Inheritance
+
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+class Base                                                           //Base Class
+{
+    public:
+            int a,b;
+
+            Base()
+            {
+                cout<<"\nBase :: Constructor"<<endl;
+            }
+
+            ~Base()
+            {
+                cout<<"\nBase :: Destructor"<<endl;
+            }
+
+    private:
+            int i,j;
+
+    protected:
+            int k,l;
+};
+
+class Derived : public Base                                             //Derived Class
+{
+    public:
+            int x;
+
+            Derived()
+            {
+                x = y = z = 0;
+
+                cout<<"\nDerived :: Constructor"<<endl;
+            }
+
+            ~Derived()
+            {
+                cout<<"\nDerived :: Destructor"<<endl;
+            }
+
+    private:
+            int y;
+
+    protected:
+            int z;
+};
+
+
+//main() Function Definition
+
+int main()
+{
+    Derived Obj;
+    getch();
+
+    Obj.a = 21;
+    Obj.b = 51;
+
+    //Obj.i = 50;                 //Not Allowed Bcoz Private
+    //Obj.j = 60;                 //Not Allowed Bcoz Private
+
+    //Obj.k = 100;                //Not Allowed Bcoz Protected
+    //Obj.l = 105;                //Not Allowed Bcoz Protected
+
+    Obj.x = 5;
+
+    //Obj.y = 10;                 //Not Allowed Bcoz Private
+    //Obj.z = 15;                 //Not Allowed Bcoz Protected
+
+    cout<<"\n Values => a = "<< Obj.a <<" b = "<< Obj.b <<" x = "<<Obj.x<<endl;
+
+    cout<<"\n Size Of Given Object => "<<sizeof(Obj)<<endl;         //36 =>As Size Of Obj = Sum Of All Non-Static Characteristics of Base as Well as derived
+                                                                    //i.e a,b,i,j,k,l => Inside Base Class 6 int Characteristics = 6 * 4 => 24 Bytes
+                                                                    // & x,y,z => Inside Derived Class 3 int Characteristics = 3 * 4 = 12 Bytes
+
+
+    getch();
+    return 0;
+}
